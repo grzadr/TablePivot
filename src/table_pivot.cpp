bf3685a01a72292f4a390bc81bace6403ec620ff
Adrian Grzemski <adrian.grzemski@gmail.com>
2019-04-15 12:40:14 +0200

    Initial commit

---
+++ src/table_pivot.cpp
@@ -0,0 +1,11 @@
+#include "agizmo/files.hpp"
+#include "agizmo/args.hpp"
+
+using AGizmo::Files::FileReader;
+using AGizmo::Args::Arguments;
+
+int main(int argc, char *argv[]){
+  Arguments args;
+
+  return 0;
+}
